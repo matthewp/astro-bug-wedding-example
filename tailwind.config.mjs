04be20193984b1e1f97c583619a2ae322b4515a2
George Fairbairn <george.fair@icloud.com>
2024-01-28 19:06:35 +0000

    feat: add button; add home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   theme: {
     extend: {
       animation: {
         'sparkle-spin': 'spinning 2000ms linear',
         'sparkle-come-in-out': 'comeInOut 1400ms forwards'
+      },
+      colors: {
+        'custom-pink': '#FFB0B0',
+        'custom-red': '#FF0000'
+      },
+      fontFamily: {
+        'rubik-mono': ['Rubik Mono One', 'sans-serif']
       },
       keyframes: {
         spinning: {
