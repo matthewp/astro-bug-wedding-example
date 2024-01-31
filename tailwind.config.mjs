a2eb66aaadd91f9729db2fdc9d081d29082336be
George Fairbairn <george.fair@icloud.com>
2024-01-31 13:02:04 +0000

    wip: add animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,10 @@ export default {
     extend: {
       animation: {
         'sparkle-spin': 'spinning 2000ms linear',
-        'sparkle-come-in-out': 'comeInOut 1400ms forwards'
+        'sparkle-come-in-out': 'comeInOut 1400ms forwards',
+        'dance': 'dance 600ms infinite 200ms',
+        'wiggle': 'wiggle 1.25s linear infinite alternate',
+        'wiggle-delay': 'wiggle 1s linear infinite alternate 500ms',
       },
       colors: {
         'custom-pink': '#FFB0B0',
@@ -23,7 +26,26 @@ export default {
         comeInOut: {
           '0%, 100%': { transform: 'scale(0)' },
           '50%': { transform: 'scale(1)' }
-        }
+        },
+        dance: {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            'animation-timing-function': 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+              transform: 'none',
+              'animation-timing-function': 'cubic-bezier(0,0,0.2,1)',
+          }
+        },
+        spin: {
+          '0%': { transform: 'rotateY(0deg)' },
+          '50%': { transform: 'rotateY(180deg)' },
+        },
+        wiggle: {
+          '0%': { transform: 'translateX(-1rem)' },
+          '50%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(1rem)' },
+        },
       }
     }
   },
