ad614c6eaa4c81e2006e719e4b9695f493fd65e7
George Fairbairn <george.fair@icloud.com>
2024-09-06 16:22:33 +0100

    try with no netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
-  integrations: [tailwind(), react()],
-  vite: {
-    esbuild: {
-      platform: 'node',
-      target: 'node20'
-    }
-  }
+  integrations: [tailwind(), react()]
 });
