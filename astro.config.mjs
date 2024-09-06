3a1cb340abb3f9707f99028176f53ddfc8563a9e
George Fairbairn <george.fair@icloud.com>
2024-09-06 15:40:22 +0100

    refactor: use correct import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 
 // https://astro.build/config
