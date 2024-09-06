78c770744c0cf94fe676d8d19eb89217957bd167
George Fairbairn <george.fair@icloud.com>
2024-09-06 15:04:20 +0100

    feat: add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [tailwind(), react()]
 });
