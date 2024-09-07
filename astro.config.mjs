06a48457323415385ecd49044d1d109762323a56
Matthew Phillips <matthew@skypack.dev>
2024-09-07 09:05:28 -0400

    Configure an integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), {
+    name: 'my-integration',
+    hooks: {
+      'astro:build:start': ({ logger }) => {
+        logger.info("Building");
+      }
+    }
+  }],
   adapter: netlify()
 });
